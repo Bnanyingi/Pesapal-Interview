8e567c55cd09bd3d28152995f6f1b799de6c220d
Barbara Nanyingi <58061814+Bnanyingi@users.noreply.github.com>
2022-01-10 08:28:08 +0300

    firstcommit

---
+++ dict.py
@@ -0,0 +1,23 @@
+text = open("sample.txt","r")
+
+d = dict()
+
+for line in text:
+    line = line.strip()
+
+    line = line.lower()
+
+    words = line.split("")
+
+    for word in words:
+        #Check if word is already in dictionary
+        if word in d:
+
+            d[word] = d[word] + 1
+
+        else:
+            d[word] = 1
+
+
+for key in list(d.keys()):
+    print(key, ":", d[key])
